600964022b4072a72ea0ee1b38b9ab5c6042232f
Dmitriy <dmitriy16doronin@gmail.com>
2020-07-25 10:33:03 +0700

    Initial commit

---
+++ NoteManager/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NoteManager.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public String Get()
+        {
+            _logger.LogDebug("Open Home page!");
+            return "Home Page";
+        }
+    }
+}
