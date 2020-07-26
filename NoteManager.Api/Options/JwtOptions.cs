2cdc76ac677c641acb10993a7d67fd3ddc525ac3
Dmitriy <dmitriy16doronin@gmail.com>
2020-07-26 18:50:27 +0700

    + swaggerOptions, jwtOptions

---
+++ NoteManager.Api/Options/JwtOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace NoteManager.Api.Options
+{
+    public class JwtOptions
+    {
+        public string Secret { get; set; }
+        public string Issure { get; set; }
+        public string Audience { get; set; }
+    }
+}
--- NoteManager.Api/Startup.cs
+++ NoteManager.Api/Startup.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -5,6 +8,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using NoteManager.Api.Options;
 using NoteManager.Application.Data;
 using NoteManager.Application.Models;
 using Npgsql;
@@ -20,24 +27,67 @@ namespace NoteManager.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //jwt 
+            services.Configure<JwtOptions>(Configuration.GetSection("JwtOptions"));
+            services.Configure<SwaggerOptions>(Configuration.GetSection("SwaggerOptions"));
+            var jwtOptions = new JwtOptions();
+            Configuration.Bind(nameof(jwtOptions), jwtOptions);
+            
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtOptions.Secret)),
+ 
+                        ValidateIssuer = true,
+                        ValidIssuer = jwtOptions.Issure,
+ 
+                        ValidateAudience = true,
+                        ValidAudience = jwtOptions.Audience,
+ 
+                        ValidateLifetime = true,
+ 
+                        ClockSkew = TimeSpan.FromSeconds(5)
+                    };
+                });
+            
+            //sql
             services.AddEntityFrameworkNpgsql().AddDbContext<AppDbContext>(options =>
                 options.UseNpgsql(new NpgsqlConnection(Configuration.GetSection("PostgreSql").GetValue<string>("ConnectionString"))));
             services.AddIdentity<User, Role>().AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
-            services.AddControllers();
             services.AddCors();
+            services.AddControllers();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("note_manager", new OpenApiInfo {Title = "Note Manager API", Version = "v1"});
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            var swaggerOptions = app.ApplicationServices.GetService<IOptions<SwaggerOptions>>().Value;
+            app.UseSwagger(options =>
+            {
+                options.RouteTemplate = swaggerOptions.JsonRoute;
+            });
+            app.UseSwaggerUI(options => options.SwaggerEndpoint(swaggerOptions.Ui, swaggerOptions.Name));
             app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseCors(cors => cors.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
             app.UseRouting();
             app.UseAuthorization();
